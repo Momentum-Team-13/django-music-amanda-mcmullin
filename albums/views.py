aca7b269cafd84d836f05c17c9955f5ecc86ec50
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-07-01 07:27:21 -0400

    all required functionality working

--- albums/views.py
+++ albums/views.py
@@ -8,21 +8,40 @@ def list_albums(request):
     return render(request, "albums/list_albums.html", {"albums": albums})
 
 
-# def add_album(request):
-#     if request.method == 'GET':
-#         form = AlbumForm()
-#     else:
-#         form = AlbumForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(to='list_albums')
-#     return render(request, "albums/add_album.html", {"form": form})
+def add_album(request):
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+    return render(request, "albums/add_album.html", {"form": form})
 
 
-# def view_album()
+def view_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, "albums/view_album.html", {"album": album})
 
 
-# def edit_album()
+def edit_album(request,pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'GET':
+        form = AlbumForm(instance=album)
+    else:
+        form = AlbumForm(data=request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+    return render(request, "albums/edit_album.html", {
+        "form": form,
+        "album": album
+    })
 
 
-# def delete_album()
+def delete_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'POST':
+        album.delete()
+        return redirect(to='list_albums')
+    return render(request, "albums/delete_album.html", {"album": album})
